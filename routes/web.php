91a0d7dd10432fba113dbb5c24e42c4078c08604
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-11-10 23:06:34 +0800

    用 new 的方式新增一筆資料

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,9 +17,15 @@ use App\Http\Controllers\PostsController;
 
 Route::get('/', function () {
     return view('welcome');
+    $post=new Post();
+    $post->title ='test title';
+    $post->content='test content';
+    $post->save();
 });
 
 Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [PostsController::class, 'show'])->name('posts.show');
 Route::get('contact', [PostsController::class, 'contact'])->name('posts.contact');
 Route::get('about', [PostsController::class, 'about'])->name('posts.about');
+
+
