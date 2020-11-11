ab87bb678b1559f5e5d24dd9b4fd31c41e2e7205
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-11-11 10:59:38 +0800

    刪除資料使用 delete 方法

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,12 @@ Route::get('/', function () {
 //        'title'=>'updated title',
 //        'content'=>'updated content',
 //    ]);
+//    $post=Post::find(1);
+//    $post->title='saved title';
+//    $post->content='saved content';
+//    $post->save();
     $post=Post::find(1);
-    $post->title='saved title';
-    $post->content='saved content';
-    $post->save();
+    $post->delete();
 
 
 
