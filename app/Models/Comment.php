a859898897db046e85ecbf0939d12de93c5dd674
HSIAO,KAI-FU <Tokizakimadthree@gmail.com>
2020-11-16 20:16:18 +0800

    開啟 Comment.php，增加關聯

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Post;
+
 
 class Comment extends Model
 {
@@ -12,6 +12,9 @@ class Comment extends Model
     protected $fillable =[
         'content'
     ];
-    
+    public function post()
+    {
+      return $this->belongsTo(Post::class);
+    }
 }
 
